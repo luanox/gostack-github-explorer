bdcf91b7b368f1dd3c30b2dbdbc23202e79b3d4b
Luan Neves <luanuemg@hotmail.com>
2020-07-04 20:49:08 -0300

    create: styles page dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 };
 
