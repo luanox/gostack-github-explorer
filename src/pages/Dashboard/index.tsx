14b4bd2a3f79f257ed49245ed19aea63e195fa3c
Luan Neves <luanuemg@hotmail.com>
2020-07-05 06:43:55 -0300

    fix: styles dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explore" />
+      <Title>Explore repositorios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
